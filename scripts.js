8e3c71a34a3f4d45b796f696feda31fe30123459
aleksejsss <aleksejssematjuks@gmail.com>
2024-06-27 15:03:16 +0300

    Revert "Merge branch 'main' of https://github.com/Xidaru/WebGame"
    This reverts commit 02d9722c321747d194df75fcb238af8683a17d31, reversing
    changes made to 6e01f160e0274df62e93f1d585556b7dfe739a42.

--- scripts.js
+++ scripts.js
@@ -3,14 +3,8 @@ $(document).ready(function(){
 $("#guest_menu").click(function() {
     window.location.href = "home.html";
 });
-});
 
-function Registerlink(){
-    window.location.href = "register.html";
-}
-function Loginlink(){
-    window.location.href = "login.html";
-}
+});
 var audio = new Audio('Audio/Dungeon Shadows.mp3');
 audio.loop = true; 
 audio.autoplay = true;
